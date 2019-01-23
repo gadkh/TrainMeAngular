831e8e7b2204ce7076a77bdac3561be821207a9b
Gadkh2 <gadkh2@gmail.com>
2019-01-23 13:28:05 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import{AngularFireDatabase}from'angularfire2/database';
+import { observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TrainMe';
+  courses:any[];
+  constructor(db:AngularFireDatabase){
+    db.list('/Courses').valueChanges().subscribe(c=>console.log(c));
+  }
 }
